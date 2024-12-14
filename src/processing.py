c0b87cbf643761283ea0680e38fbf7eeb8767831
Arisha108 <a.heubschman@gmail.com>
2024-12-13 18:09:34 -0600

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,8 @@
-def filter_by_state(my_list, state='EXECUTED'):
+def filter_by_state(my_list:list, state:str='EXECUTED') -> list:
+    """
+    Функция, которая принимает на вход список словарей
+    и возвращает отфильтрованный список по ключу state
+    """
     new_data = []
     for i in my_list:
         if i['state'] == state:
@@ -6,7 +10,11 @@ def filter_by_state(my_list, state='EXECUTED'):
     return new_data
 
 
-def sort_by_date(my_list, reverse=True):
+def sort_by_date(my_list:list, reverse:bool=True) -> list:
+    """
+    Функция, котооая принимает на вход список словарей
+    и возвращает отсортированный список по ключу date
+    """
     new_data = sorted(my_list, key=lambda x: x['date'], reverse=reverse)
 
     return new_data
